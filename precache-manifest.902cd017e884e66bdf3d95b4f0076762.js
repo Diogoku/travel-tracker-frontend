ae4ff5b44d32c80901c42567b17e84ec809f9a64
diogoku <diogomnribeiro@gmail.com>
2020-10-15 12:55:42 +0100

    Updates

---
+++ precache-manifest.902cd017e884e66bdf3d95b4f0076762.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "706f1ddd5780881b9383f6ba0b767e1a",
+    "url": "/travel-tracker-frontend/index.html"
+  },
+  {
+    "revision": "1c4148b08938ae342b1c",
+    "url": "/travel-tracker-frontend/static/css/main.13156c54.chunk.css"
+  },
+  {
+    "revision": "437570938d89b9c2ac87",
+    "url": "/travel-tracker-frontend/static/js/2.cbe338a3.chunk.js"
+  },
+  {
+    "revision": "4dc64096409ca8bce897af29b30ab117",
+    "url": "/travel-tracker-frontend/static/js/2.cbe338a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c4148b08938ae342b1c",
+    "url": "/travel-tracker-frontend/static/js/main.398d8dc1.chunk.js"
+  },
+  {
+    "revision": "5754c76d3690aaaaf8da",
+    "url": "/travel-tracker-frontend/static/js/runtime-main.bbd642ee.js"
+  }
+]);
